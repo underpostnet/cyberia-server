2ade36b73b13250be5fe0668de871fa3e18ac027
underpostnet <fcoverdugoa@underpost.net>
2025-05-26 17:05:26 -0400

    update MAZE_CELL_WORLD_SIZE

--- instance/constanst.go
+++ instance/constanst.go
@@ -7,7 +7,7 @@ const (
 	WORLD_WIDTH            = 1600                  // Instance world width in pixels
 	WORLD_HEIGHT           = 1600                  // Instance world height in pixels
 	OBJECT_SIZE            = 50                    // Size of instance objects (players, obstacles) in pixels
-	MAZE_CELL_WORLD_SIZE   = WORLD_WIDTH / 32      // Size of a maze cell for A* pathfinding (50x50 pixels)
+	MAZE_CELL_WORLD_SIZE   = 32                    // Size of a maze cell for A* pathfinding (50x50 pixels)
 	INSTANCE_TICK_INTERVAL = 50 * time.Millisecond // Instance state update interval (20 frames per second)
 )
 
